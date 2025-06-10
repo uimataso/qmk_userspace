3fb3501460d6fbdcc0d3bee18c7479cff3c92d0c
uima <git.m5svm@uimataso.com>
2025-06-10 10:50:12 +0800

    make num layer and def laer consistent

--- keyboards/ferris/sweep/keymaps/uima_sweep/keymap.c
+++ keyboards/ferris/sweep/keymaps/uima_sweep/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUM_] = LAYOUT(
         XXXXXXX, KC_7,    KC_8,    KC_9,    KC_TILD,      KC_PIPE, KC_CIRC, KC_ASTR, KC_SLSH, XXXXXXX,
         KC_GT,   KC_4,    KC_5,    KC_6,    KC_0,         KC_AMPR, KC_PLUS, KC_MINS, KC_EQL,  KC_PERC,
-        KC_LT,   KC_1,    KC_2,    KC_3,    KC_EXLM,      _______, KC_RBRC, KC_LBRC, KC_COMM, KC_DOT,
+        KC_LT,   KC_1,    KC_2,    KC_3,    KC_EXLM,      _______, KC_RBRC, KC_LBRC, KC_DOT,  KC_SLSH,
                                    _______, _______,      _______, _______
     ),
 
