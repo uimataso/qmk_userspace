75ef662c3b1a598d4aed26165e6596ee2afc7895
uima <git.m5svm@uimataso.com>
2025-10-08 10:21:32 +0800

    add tty binding

--- keyboards/ferris/sweep/keymaps/uima_sweep/keymap.c
+++ keyboards/ferris/sweep/keymaps/uima_sweep/keymap.c
@@ -9,6 +9,8 @@ enum layers_names {
     _FUN_,
 };
 
+#define TTY(n) A(C(KC_F##n))
+
 enum custom_keycodes {
     KEYCODE = SAFE_RANGE,
 
@@ -73,9 +75,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_FUN_] = LAYOUT(
-        XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F10,       KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
-        KC_PSCR, KC_F4,   KC_F5,   KC_F6,   KC_F11,       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F12,       S_LEFT,  S_DOWN,  S_UP,    S_RGHT, _______,
+        XXXXXXX, TTY(7),  TTY(8),  TTY(9),  _______,      KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX,
+        KC_PSCR, TTY(4),  TTY(5),  TTY(6),  _______,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+        _______, TTY(1),  TTY(2),  TTY(3),  _______,      S_LEFT,  S_DOWN,  S_UP,    S_RGHT, _______,
                                    XXXXXXX, _______,      _______, XXXXXXX
     ),
 };
